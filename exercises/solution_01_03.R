3544ce8de02325ea9edcf90959fad35164e97b4c
Ines Montani <ines@ines.io>
2019-04-18 13:26:01 +0200

    Update code

--- exercises/solution_01_03.R
+++ exercises/solution_01_03.R
@@ -1,13 +1,19 @@
 library(rjson)
 library(ggplot2)
 
-# This code will run relative to the root of the repo, so we can load files
-data <- fromJSON(file = "exercises/mtcars.json")
+mtcars$gear <- factor(mtcars$gear,levels=c(3,4,5),
+  	labels=c("3gears","4gears","5gears"))
+mtcars$am <- factor(mtcars$am,levels=c(0,1),
+  	labels=c("Automatic","Manual"))
+mtcars$cyl <- factor(mtcars$cyl,levels=c(4,6,8),
+   labels=c("4cyl","6cyl","8cyl"))
 
-# Print the first record in the data
-print(data[1])
+# Print the first record in mtcars
+print(mtcars[1])
 
-# Assign the length of data to some_var
-some_var <- length(data)
+# Assign the length of mtcars to some_var
+some_var <- length(mtcars)
 
-qplot(mpg, data = data)
+print(qplot(mpg, data=mtcars, geom="density", fill=gear, alpha=I(.5),
+   main="Distribution of Gas Milage", xlab="Miles Per Gallon",
+   ylab="Density"))
